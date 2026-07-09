3e1706cb4ac32065490c4b7e6d9f888650109947
mingxueyu <mingxueyu@users.noreply.github.com>
2026-07-10 01:32:24 +0800

    新增时间线页面、背景音乐播放器、毛玻璃半透明模块、背景图全屏优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,11 @@ export default defineConfig({
       theme: "github-dark",
     },
   },
+  vite: {
+    server: {
+      watch: {
+        ignored: ["**/public/audio/**"],
+      },
+    },
+  },
 });
